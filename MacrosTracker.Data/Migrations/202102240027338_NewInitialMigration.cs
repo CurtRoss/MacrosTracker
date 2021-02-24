d047705063bbc4fd6964799478061b38e3f713d8
unknown <lindseyjrobertson@gmail.com>
2021-02-23 19:30:01 -0500

    FixedMirgrations

--- MacrosTracker.Data/Migrations/202102240027338_NewInitialMigration.cs
+++ MacrosTracker.Data/Migrations/202102240027338_NewInitialMigration.cs
@@ -1,20 +1,60 @@
-namespace MacrosTracker.Data.Migrations
+﻿namespace MacrosTracker.Data.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class InitialCreate : DbMigration
+    public partial class NewInitialMigration : DbMigration
     {
         public override void Up()
         {
+            CreateTable(
+                "dbo.Meal",
+                c => new
+                    {
+                        MealId = c.Int(nullable: false, identity: true),
+                        FoodId = c.Int(nullable: false),
+                        UserId = c.Guid(nullable: false),
+                        MealName = c.String(nullable: false, maxLength: 50),
+                        Category = c.String(),
+                        CreatedUtc = c.DateTimeOffset(nullable: false, precision: 7),
+                        ModifiedUtc = c.DateTimeOffset(nullable: false, precision: 7),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.MealId)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
             CreateTable(
                 "dbo.FoodItem",
                 c => new
                     {
                         FoodId = c.Int(nullable: false, identity: true),
                         FoodName = c.String(),
+                        Amount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Protein = c.Double(nullable: false),
+                        Fat = c.Double(nullable: false),
+                        Carbs = c.Double(nullable: false),
+                        UserId = c.Guid(nullable: false),
+                        CreatedUtc = c.DateTimeOffset(nullable: false, precision: 7),
+                        ModifiedUtc = c.DateTimeOffset(precision: 7),
+                        ApplicationUser_Id = c.String(maxLength: 128),
                     })
-                .PrimaryKey(t => t.FoodId);
+                .PrimaryKey(t => t.FoodId)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.FoodMeal",
+                c => new
+                    {
+                        MealId = c.Int(nullable: false),
+                        FoodId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.MealId, t.FoodId })
+                .ForeignKey("dbo.FoodItem", t => t.FoodId, cascadeDelete: true)
+                .ForeignKey("dbo.Meal", t => t.MealId, cascadeDelete: true)
+                .Index(t => t.MealId)
+                .Index(t => t.FoodId);
             
             CreateTable(
                 "dbo.IdentityRole",
@@ -45,10 +85,10 @@ namespace MacrosTracker.Data.Migrations
                 c => new
                     {
                         Id = c.String(nullable: false, maxLength: 128),
-                        HeightInInches = c.Int(nullable: false),
-                        Weight = c.Int(nullable: false),
+                        Height = c.Int(nullable: false),
+                        Weight = c.Double(nullable: false),
                         MaleOrFemale = c.String(nullable: false),
-                        Age = c.Int(nullable: false),
+                        DateOfBirth = c.DateTime(nullable: false),
                         Email = c.String(),
                         EmailConfirmed = c.Boolean(nullable: false),
                         PasswordHash = c.String(),
@@ -90,24 +130,52 @@ namespace MacrosTracker.Data.Migrations
                 .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
                 .Index(t => t.ApplicationUser_Id);
             
+            CreateTable(
+                "dbo.FoodItemMeal",
+                c => new
+                    {
+                        FoodItem_FoodId = c.Int(nullable: false),
+                        Meal_MealId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.FoodItem_FoodId, t.Meal_MealId })
+                .ForeignKey("dbo.FoodItem", t => t.FoodItem_FoodId, cascadeDelete: true)
+                .ForeignKey("dbo.Meal", t => t.Meal_MealId, cascadeDelete: true)
+                .Index(t => t.FoodItem_FoodId)
+                .Index(t => t.Meal_MealId);
+            
         }
         
         public override void Down()
         {
             DropForeignKey("dbo.IdentityUserRole", "ApplicationUser_Id", "dbo.ApplicationUser");
             DropForeignKey("dbo.IdentityUserLogin", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.Meal", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.FoodItem", "ApplicationUser_Id", "dbo.ApplicationUser");
             DropForeignKey("dbo.IdentityUserClaim", "ApplicationUser_Id", "dbo.ApplicationUser");
             DropForeignKey("dbo.IdentityUserRole", "IdentityRole_Id", "dbo.IdentityRole");
+            DropForeignKey("dbo.FoodMeal", "MealId", "dbo.Meal");
+            DropForeignKey("dbo.FoodMeal", "FoodId", "dbo.FoodItem");
+            DropForeignKey("dbo.FoodItemMeal", "Meal_MealId", "dbo.Meal");
+            DropForeignKey("dbo.FoodItemMeal", "FoodItem_FoodId", "dbo.FoodItem");
+            DropIndex("dbo.FoodItemMeal", new[] { "Meal_MealId" });
+            DropIndex("dbo.FoodItemMeal", new[] { "FoodItem_FoodId" });
             DropIndex("dbo.IdentityUserLogin", new[] { "ApplicationUser_Id" });
             DropIndex("dbo.IdentityUserClaim", new[] { "ApplicationUser_Id" });
             DropIndex("dbo.IdentityUserRole", new[] { "ApplicationUser_Id" });
             DropIndex("dbo.IdentityUserRole", new[] { "IdentityRole_Id" });
+            DropIndex("dbo.FoodMeal", new[] { "FoodId" });
+            DropIndex("dbo.FoodMeal", new[] { "MealId" });
+            DropIndex("dbo.FoodItem", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.Meal", new[] { "ApplicationUser_Id" });
+            DropTable("dbo.FoodItemMeal");
             DropTable("dbo.IdentityUserLogin");
             DropTable("dbo.IdentityUserClaim");
             DropTable("dbo.ApplicationUser");
             DropTable("dbo.IdentityUserRole");
             DropTable("dbo.IdentityRole");
+            DropTable("dbo.FoodMeal");
             DropTable("dbo.FoodItem");
+            DropTable("dbo.Meal");
         }
     }
 }
